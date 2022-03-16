f38465a850501262a7ff56f5fc2026d759ecd416
JosephChen1993 <ooo93301@gmail.com>
2022-03-16 19:30:12 +0000

    part A completed

--- CountedElement.java
+++ CountedElement.java
@@ -1,25 +1,52 @@
+
+/**
+ * @author 2669729C Ching-Yuan Chen
+ */
+
 public class CountedElement<E extends Comparable<E>> implements Comparable<CountedElement<E>> {
 	private E element;
 	private int count;
 
 	public CountedElement(E e, int count){
 		//constructor - to complete
+		this.element = e;
+		this.count = count;
 	}
 	
 	public CountedElement(E e){
 		//constructor - to complete
+		this.element = e;
+		this.count = 1;
 	}
 
 	//add getters and setters
+	// element getter method
+	public E getElement() {
+		return this.element;
+	}
+	// element setter method
+	public void setElement( E element ) {
+		this.element = element;
+	}
+	// count getter method
+	public int getCount() {
+		return this.count;
+	}
+	// count setter method
+	public void setCount( int count ) {
+		this.count = count;
+	}
 	
 	//add toString() method
-	
+	public String toString() {
+		return "(" + element + "," + count + ")";
+	}
 	
 
 	
 	public int compareTo(CountedElement<E> sC1) {
-    
 		//to complete
+		return this.element.compareTo( sC1.element );
 	}
 
 }
--- WordProcessor.java
+++ WordProcessor.java
@@ -1,10 +1,15 @@
 
 //import classes for file input - scanner etc.
+import java.util.Scanner;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Set;
 //import implementing set (eg. TreeSet)
+import java.util.TreeSet;
 
-
+/**
+ * @author 2669729C Ching-Yuan Chen
+ */
 
 
 public class WordProcessor {
@@ -12,22 +17,55 @@ public class WordProcessor {
 		//implement this static method to create a
 		// String representation of set - 5 comma separated elements per line
 		// assume that type E has a toString method
+		String ret = "";
+		int cnt = 0;
+		for( E e : inputSet ) {
+			ret += (e.toString() + ", ");
+			cnt++;
+			// newline if this line has 5 elements
+			if( cnt == 5 ) {
+				ret += "\n";
+				cnt = 0;
+			}
+		}
+		return ret;
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// args = new String[]{"file0.txt", "file1.txt", "file2.txt"};
 		//create a set of type String called wordSet
+		Set<String> wordSet = new TreeSet<>();
 		//create a set of type CountedElement<String> called countedWordSet 
+		TreeSet<CountedElement<String>> countedWordSet = new TreeSet<>();
 		
 		//for each input file (assume 3 arguments, each the name of a file)
-		//  for each word w
-		//     if wordset doesnt contain w:
-		//        add w to wordset
-	    //        add new element to countedWordSet
-		//     else
-		//        increment numeric part of element in countedWordSet containing w
+		for( String fn : args ) {
+			// open file
+			try (
+				Scanner sc = new Scanner( new File( fn ) );
+			) {
+			//  for each word w
+				while( sc.hasNext() ) {
+					String wd = sc.next();
+			//     if wordset doesnt contain w:
+					if( !wordSet.contains( wd ) ) {
+			//        add w to wordset
+						wordSet.add( wd );
+			//        add new element to countedWordSet
+						countedWordSet.add( new CountedElement<String>( wd ) );
+					}
+			//     else
+					else {
+			//        increment numeric part of element in countedWordSet containing w
+						var ce = countedWordSet.floor( new CountedElement<String>( wd ) );
+						ce.setCount( ce.getCount() + 1 );
+					}
+				}
+			} catch ( FileNotFoundException e ) { e.printStackTrace(); }
+		}
 		
 
 	System.out.println(displaySet(countedWordSet));
